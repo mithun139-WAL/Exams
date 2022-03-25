d91593fb1e9c5f00e7874b85c457aaab5a30aa63
mithun139 <mithunchakravarthi139@gmail.com>
2022-03-25 10:42:51 +0530

    Added files

---
+++ Exam_25March2022/React/src/App.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+// eslint-disable-next-line object-curly-spacing
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Author from './Author';
+import Photos from './components/Photos';
+import Todos from './Todos';
+import User from './User';
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            UserApp
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link to="/todos" className="nav-item nav-link">
+                Add Todos
+              </Link>
+              <Link to="/authors" className="nav-item nav-link">
+                Authors
+              </Link>
+              <Link to="/user" className="nav-item nav-link">
+                Users
+              </Link>
+              <Link to="/photos" className="nav-item nav-link">
+                Photos
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/todos" element={<Todos />}></Route>
+          <Route path="/authors" element={<Author />}></Route>
+          <Route path="/user" element={<User />}></Route>
+          <Route path="/photos" element={<Photos />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
